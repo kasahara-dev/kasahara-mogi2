80135b2c0a230bd180df8690e9b3cfdaec84e29c
kasahara <kasahara.maki.info@gmail.com>
2025-11-11 20:50:53 +0900

    コントローラ

---
+++ src/app/Http/Controllers/EmailVerificationController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+
+
+class EmailVerificationController extends Controller
+{
+    public function show()
+    {
+        return view('auth.verify');
+    }
+}
