6a3dca7a885aa9ce47f0252fd5702cdc9ff467b9
kasahara <kasahara.maki.info@gmail.com>
2025-11-12 13:32:00 +0900

    管理者ログイン

--- src/routes/web.php
+++ src/routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\ItemController;
 use App\Http\Controllers\PurchaseController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\AdminLoginController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\AddressController;
 
@@ -40,15 +41,11 @@ Route::middleware('auth')->group(function () {
 });
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/attendance', [AttendanceController::class, 'show']);
-    Route::post('/item/{item_id}', [CommentController::class, 'store']);
-    Route::delete('/item/{item_id}', [CommentController::class, 'destroy']);
-    Route::get('/purchase/{item_id}', [PurchaseController::class, 'create'])->name('purchase');
-    Route::post('/purchase/{item_id}', [PurchaseController::class, 'store'])->name('purchase.store');
-    Route::get('/purchase/address/{item_id}', [AddressController::class, 'create']);
-    Route::post('/purchase/address/{item_id}', [AddressController::class, 'store']);
-    Route::get('/sell', [ItemController::class, 'create']);
-    Route::post('/sell', [ItemController::class, 'store']);
-    Route::get('/mypage', [ProfileController::class, 'show']);
-    Route::get('/mypage/profile', [ProfileController::class, 'edit']);
-    Route::put('/mypage/profile', [ProfileController::class, 'update']);
+});
+Route::prefix('admin')->group(function () {
+    Route::get('/login', [AdminLoginController::class, 'create'])->name('admin.login');
+    Route::post('/login', [AdminLoginController::class, 'store']);
+    Route::middleware('auth:admin')->group(function () {
+        Route::get('/attendance/list', [AttendanceController::class, 'index']);
+    });
 });
